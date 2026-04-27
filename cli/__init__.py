5b337e59534f061ac62c8c6db112b087e46115aa
azizifaisal33-dotcom <azizifaisal33@gmail.com>
2026-04-28 06:45:49 +0700

    Update __init__.py

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,17 +1,1 @@
-"""
-💻 Nano CLI Framework v2
-"""
-__version__ = "2.0.0"
-
-# Lazy imports - Production style
-def get_shell(ai_instance):
-    from .shell import NanoShell
-    return NanoShell(ai_instance)
-
-def run_cli():
-    from .runner import main
-    main()
-
-__all__ = ["get_shell", "run_cli"]
-
-print("💻 CLI Framework loaded ✓")
+# CLI interface
