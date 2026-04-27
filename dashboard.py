e75579223fdb692120d1f00570c2faf07dfb651c
azizifaisal33-dotcom <azizifaisal33@gmail.com>
2026-04-27 16:55:00 +0700

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,8 @@
+from rich.live import Live
+from rich.layout import Layout
+
+def live_dashboard():
+    layout = Layout()
+    # CPU/RAM/Battery live graphs
+    # Plugin browser
+    # Command history
