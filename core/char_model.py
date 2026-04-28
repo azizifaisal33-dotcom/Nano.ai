62ce534b01811a8192a02eb86bb2f6a7fa4b7ee4
azizifaisal33-dotcom <azizifaisal33@gmail.com>
2026-04-28 08:14:31 +0700

    Update char_model.py

--- core/char_model.py
+++ core/char_model.py
@@ -1,37 +1,29 @@
 import random
 from collections import defaultdict
 
-class CharModel:
+class CharAI:
     def __init__(self):
         self.chain = defaultdict(list)
 
-    # =========================
-    # TRAINING
-    # =========================
     def train(self, text):
         text = text.lower()
 
         for i in range(len(text) - 1):
-            current = text[i]
-            next_char = text[i + 1]
+            a = text[i]
+            b = text[i + 1]
 
-            if current.isalpha():
-                self.chain[current].append(next_char)
+            if a.isalpha():
+                self.chain[a].append(b)
 
-    # =========================
-    # GENERATE TEXT
-    # =========================
     def generate(self, start="h", length=10):
         result = start
-
         current = start
 
         for _ in range(length):
             if current not in self.chain:
-                current = random.choice(list(self.chain.keys()))
+                break
 
             next_chars = self.chain[current]
-
             if not next_chars:
                 break
 
