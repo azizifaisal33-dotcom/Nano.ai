48f91ade42a68cdf6ecb2e8865a91a5338be2cc5
azizifaisal33-dotcom <azizifaisal33@gmail.com>
2026-04-28 07:29:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-py
+from core.brain import Brain
+
+if __name__ == "__main__":
+    brain = Brain()
+
+    print("\n💬 Nano AI ACTIVE\n")
+
+    while True:
+        try:
+            user = input("you> ")
+
+            if user in ["exit", "quit"]:
+                print("bye")
+                break
+
+            print("\n🧠 AI:\n", brain.think(user), "\n")
+
+        except KeyboardInterrupt:
+            print("\nbye")
+            break
+
+        except Exception as e:
+            print("⚠️ error:", e)
